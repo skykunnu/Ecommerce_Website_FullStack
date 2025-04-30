c8020cfeaea167d89a71ce9232eaa5799b2b85df
Shikhar Khandelwal <skykunnu@gmail.com>
2025-04-30 17:06:24 +0530

    Removed comments from Product page

--- Back/controllers/Product.js
+++ Back/controllers/Product.js
@@ -41,7 +41,6 @@ export async function fetchProduct(req, res) {
   try {
     let query = {};
     if (req.params.slug) {
-      // query._id = req.params.id;
       query.slug = req.params.slug;
     }
     console.log("req.params.slug",req.params.slug);
@@ -58,7 +57,7 @@ export async function fetchProduct(req, res) {
     }
 
     const page = req.query.page ? Number(req.query.page) : 1;
-    // console.log("page in fetchProduct", page);
+  
     const limit = 10;
     const skip = (page - 1) * limit;
 
@@ -88,7 +87,6 @@ export async function fetchCategories(req, res) {
     let query = {};
 
     const page = req.query.page ? Number(req.query.page) : 1;
-    // console.log("page in fetchCategories", page);
     const limit = 10;
     const skip = (page - 1) * limit;
 
